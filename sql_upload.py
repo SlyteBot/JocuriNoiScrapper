e491ec244dec776a1418c3089f488348d3df3f24
Slyte <siposszilard@ymail.com>
2023-10-23 22:59:15 +0300

    SQL insert genres

--- sql_upload.py
+++ sql_upload.py
@@ -1,11 +1,12 @@
 from mysql.connector import connect, Error
+from game import Game
 
 
 class SQL:
-    def __init__(self):
 
+    def connection(self):
         try:
-            self.connection = connect(
+            return connect(
                 host="localhost",
                 user="root",
                 database="webshop"
@@ -13,12 +14,25 @@ class SQL:
         except Error as e:
             print(e)
 
-    def connection(self):
-        return connect(
-            host="localhost",
-            user="root",
-        )
+    def extract_genres(self, games: list[Game]):
+        genres = []
+        for game in games:
+            for genre in game.genres:
+                if genre not in genres:
+                    genres.append(genre)
+        add_genre = ("INSERT INTO genres"
+                     "(genre_name)"
+                     "VALUES (%s);")
+        with self.connection() as connection:
+            cursor = connection.cursor()
+            for genre in genres:
+                cursor.execute(add_genre, [genre])
+            connection.commit()
+            cursor.close()
 
+
+    def insert_prdocuts
 
 if __name__ == '__main__':
     instance = SQL()
+    instance.extract_genres([])
